1fb4784f7e927c6eec6da2c55c617c302dfc4d79
brettwatty <Brett.Watt@niwa.co.nz>
2020-08-13 13:53:32 +1200

    Ignore

--- dump/ASCDMega8x/ASCDMega8x.h
+++
@@ -1,8 +0,0 @@
-#ifndef ASCD_MEGA_8X_H
-#define ASCD_MEGA_8X_H
-#include <Arduino.h>
-// #include <ReadInputMega8x.h>
-// #include <WriteOutputMega8x.h>
-// #include <Variables.h>
-#include <CycleFunctions.h>
-#endif
--- dump/ASCDNano4x/ASCDNano4x.h
+++
@@ -1,8 +0,0 @@
-#ifndef ASCD_NANO_4X_H
-#define ASCD_NANO_4X_H
-#include <Arduino.h>
-// #include <ReadInputNano4x.h>
-// #include <WriteOutputNano4x.h>
-// #include <Variables.h>
-#include <CycleFunctions.h>
-#endif
